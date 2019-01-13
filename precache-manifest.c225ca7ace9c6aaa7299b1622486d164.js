3d03beb4023c71e6486a2e788390f49215d724ee
oleg <oleg.kupriianov@binary-studio.com>
2019-01-13 14:58:31 +0200

    Updates

---
+++ precache-manifest.c225ca7ace9c6aaa7299b1622486d164.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1337d597627964789b6",
+    "url": "/twitter-battle-ts/static/js/runtime~main.e1337d59.js"
+  },
+  {
+    "revision": "f3a1dbda5a4f0fe387e9",
+    "url": "/twitter-battle-ts/static/js/main.f3a1dbda.chunk.js"
+  },
+  {
+    "revision": "d6e6a80e96d817f27091",
+    "url": "/twitter-battle-ts/static/js/1.d6e6a80e.chunk.js"
+  },
+  {
+    "revision": "f3a1dbda5a4f0fe387e9",
+    "url": "/twitter-battle-ts/static/css/main.57c8e9b2.chunk.css"
+  },
+  {
+    "revision": "8f4e5dca997f47b639d0794c7f5b14f7",
+    "url": "/twitter-battle-ts/index.html"
+  }
+];
